930210d021f7334365b17ed9c09d300f6fa16cf4
shamsher <shamsher.kc.c4@gmail.com>
2025-01-07 11:40:18 +0900

    added navbar and digital clock project

---
+++ smallProjects/digitalClock/js/main.js
@@ -0,0 +1,25 @@
+function clock() {
+  let hour = document.getElementById("hour");
+  let minute = document.getElementById("minute");
+  let second = document.getElementById("second");
+  let period = document.getElementById("period");
+
+  const date = new Date();
+  const h = date.getHours();
+  const m = date.getMinutes();
+  const s = date.getSeconds();
+
+  const ampm = h >= 12 ? "PM" : "AM";
+
+  if (h > 12) {
+    h = h - 12;
+  }
+
+  hour.innerText = h;
+  minute.innerText = m;
+  second.innerText = s;
+  hour.innerText = h;
+  period.innerText = ampm;
+}
+
+setInterval(clock, 1000);
---
+++ smallProjects/navBar/js/main.js
@@ -0,0 +1,3 @@
+console.log("2025");
+let faef = document.getElementById("para");
+console.log(faef);
