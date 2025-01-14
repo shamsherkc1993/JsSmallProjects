ba5875ef5fb0912e0620f3178e36b3257d2a0798
shamsher <shamsher.kc.c4@gmail.com>
2025-01-14 13:29:14 +0900

    refined code

--- smallProjects/todoList/js/main.js
+++ smallProjects/todoList/js/main.js
@@ -1,44 +1,23 @@
-// function inputFunction(e) {
-//   let newValue = e.value;
-//   let splitedValue = newValue.split(" ");
-//   document.getElementById("newText").innerHTML = newValue;
-//   document.getElementById("character").innerHTML = newValue.length;
-//   document.getElementById("word").innerHTML = splitedValue.length;
+function submitForm() {
+  if (document.getElementById("inputTask").value == "") {
+    window.alert("Please input some lists");
+  } else {
+    const newTask = document.createElement("li");
+    const taskList = document.getElementById("taskLists");
+    taskList.appendChild(newTask);
+    newTask.textContent = document.getElementById("inputTask").value;
+    document.getElementById("inputTask").value = "";
+    deleteTask(newTask);
+  }
+}
 
-//   if (newValue.length >= 1) {
-//     document.getElementById("newText").style.display = "block";
-//   } else {
-//     document.getElementById("newText").style.display = "none";
-//     document.getElementById("word").innerText = 0;
-//   }
-// }
-
-// let inp = document.getElementById("inp");
-// console.log(inp.value);
-
-// function focusFunction(e) {
-//   // document.getElementById("inp").style.background = "lime";
-//   e.style.background = "lime";
-// }
-
-// function blurFunction(e) {
-//   e.style.background = "";
-// }
-
-// function inputFunction(e) {
-//   let x = e.value;
-//   document.getElementById("test").innerHTML = x;
-//   console.log(x);
-// }
-
-// function inputFunction(e) {
-//   let x = e.value;
-//   // document.getElementById("test").innerHTML = x;
-//   console.log(x);
-//   return x;
-// }
-// console.log(inputFunction());
-
-// function clickFunction() {
-//   console.log();
-// }
+function deleteTask(newTask) {
+  let deleteBtn = document.createElement("buttom");
+  deleteBtn.textContent = "X";
+  deleteBtn.classList.add("deletebutton");
+  newTask.appendChild(deleteBtn);
+  deleteBtn.onclick = function () {
+    newTask.remove();
+    window.alert("Do you want to delete this list ?");
+  };
+}
